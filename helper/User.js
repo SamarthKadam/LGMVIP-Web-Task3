b366778fe855c0b4ccb080790591acf5e2a276b6
Samarth Kadam <samarthskadam14@gmail.com>
2022-07-26 21:03:44 +0530

    changes done

--- helper/User.js
+++ helper/User.js
@@ -15,7 +15,7 @@
         <img class="logo-person" src="/user.png" alt="">
         <div class="details">
           <div class="name">${this.Name}</div>
-          <div class="email-phone"><img class="email-icon"  src="email.png" alt=""> ${this.Email} |<img class="phone-icon" src="phone-call.png" alt=""> ${this.Phone}</div>
+          <div class="email-phone"><img class="email-icon"  src="https://i.ibb.co/nRxd7pp/email.png" alt=""> ${this.Email} |<img class="phone-icon" src="https://i.ibb.co/16KtPr4/phone-call.png" alt=""> ${this.Phone}</div>
           <div class="view-details">View Details ></div>
         </div>
       </div>`
